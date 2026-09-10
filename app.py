2f95f535687f791b1fc4d5af981a91be9ccc9cdb
lquinon2 <lquinon2@terpmail.umd.edu>
2026-09-10 15:59:44 -0400

    Updated the code I added for a new route for hello HTML page.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Leslie Quinonez! I am adding my first code change.'
-app.route('/hello')
+@app.route('/hello')
 def hello():
     return render_template('hello.html')
 
